20820bca5e6cef073d074fe0ec89c11b078f66e9
claraizquierdo <claraizquierdosanz@gmail.com>
2024-01-12 19:41:53 +0100

    Add styles for header, navigation and footer

--- src/components/App.js
+++ src/components/App.js
@@ -10,20 +10,20 @@ function App() {
         <div>
             <header className="header">
                 <img src="logo.png" className="logo" alt="logo" />
-                <h1>
+                <h1 className="h1">
                     Imperial destroyers center
                 </h1>
             </header>
-            <Navigation activePage={activePage} setActivePage={setActivePage}/>
-            <main>
+            <main className="main">
+                <Navigation activePage={activePage} setActivePage={setActivePage}/>
                 {activePage === "planets" ? 
                     <Planets />
                  : 
                     <Starships />
                 }
             </main>
-            <footer>
-                The footer
+            <footer className="footer">
+                App created by <a className="link link--dark" href="https://github.com/claraizquierdo">Clara Izquierdo</a> with love
             </footer>
         </div>
     );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import '../css/Navigation.scss';
 
-const Navigation = ({setActivePage}) => {
-  return (
-    <nav className="navigation">
-      <ul>
-        <li>
-          <button onClick={() =>setActivePage("planets")}>Planets</button>
-        </li>
-        <li>
-          <button onClick={() =>setActivePage("starships")}>Starships</button>
-        </li>
-      </ul>
-    </nav>
-  );
+const Navigation = ({ activePage, setActivePage }) => {
+    return (
+        <nav className="navigation">
+            <ul>
+                <li>
+                    <button 
+                        className={activePage === "planets" ? 'active button button--as-link' : 'button button--as-link'} 
+                        onClick={() => setActivePage("planets")}
+                    >
+                        <span className="icon icon-earth" />
+                        Planets
+                    </button>
+                </li>
+                <li>
+                    <button 
+                        className={activePage === "starships" ? 'active button button--as-link' : 'button button--as-link'}
+                        onClick={() => setActivePage("starships")}
+                    >
+                        <span className="icon icon-rocket" />
+                        Starships
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navigation;
