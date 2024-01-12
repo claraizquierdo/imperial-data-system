02488f57bb48ab8f135b0907da62c1e6a65d490b
claraizquierdo <claraizquierdosanz@gmail.com>
2024-01-12 10:28:28 +0100

    Add basic structure

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import '../css/App.scss';
+
+function App() {
+  return (
+    <div>
+      <header className="header">
+        <img src="logo.png" className="logo" alt="logo" />
+        <h1>
+          Imperial destroyers center
+        </h1>
+      </header>
+      <nav>
+        The navigation
+      </nav>
+      <main>
+        The main content
+      </main>
+      <footer>
+        The footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
