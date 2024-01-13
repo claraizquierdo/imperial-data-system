b038d5a1669e8ecec4b81e426b32b5c836c28f7d
claraizquierdo <claraizquierdosanz@gmail.com>
2024-01-13 15:57:13 +0100

    Style improvements

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,12 @@
 import '../css/components/Loading.scss';
 
 const Loading = () => {
-  return (
-    <div className="loading">
-      <span>Loading...</span>
-    </div>
-  );
+    return (
+        <div className="loading">
+            <div className="loading__spinner"></div>
+            <span>Loading...</span>
+        </div>
+    );
 };
 
 export default Loading;
